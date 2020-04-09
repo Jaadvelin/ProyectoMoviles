5b215df3391c3f23e48d05342b4a1fb63a068cae
DanilN1 <36081046+DanilN1@users.noreply.github.com>
2020-04-09 17:24:18 -0600

    Initial Preparations

---
+++ RelaxingProject/app/src/main/java/com/classes/Log.kt
@@ -0,0 +1,14 @@
+package com.classes
+
+import java.util.Date
+
+class Log{
+    var title: String = ""
+    var date: Date
+        get() {
+            TODO()
+        }
+        set(value) {}
+    var text: String = ""
+
+}
