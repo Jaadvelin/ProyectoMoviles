2f8a3560154d7011fd7aa59cd644ba96cf058cc4
DanilN1 <36081046+DanilN1@users.noreply.github.com>
2020-04-13 20:34:30 -0600

    Project Reset and Some Log additions

---
+++ RelaxingProject/app/src/main/java/com/relaxingproject/classes/Log.kt
@@ -0,0 +1,26 @@
+package com.relaxingproject.classes
+
+import java.util.Date
+
+class Log{
+    var title: String = "Default"
+        get() = field
+        set(value){
+            field = value
+        }
+    var date: Date = Date(2020,1,1)
+        get() = field
+        set(value) {
+            field = value
+        }
+    /*var date: String = "01-01-2020"
+        get() = field
+        set(value) {
+            field = value
+        }*/
+    var text: String = "Default"
+        get() = field
+        set(value){
+            field = value
+        }
+}
---
+++ RelaxingProject/app/src/test/java/com/relaxingproject/LogTest.kt
@@ -0,0 +1,26 @@
+package com.relaxingproject
+
+import com.relaxingproject.classes.Log
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.Date
+class LogTest {
+    @Test
+    fun logClassSaveTitle() {
+        var log = Log()
+        log.title = "Test"
+        assertEquals("Test", log.title)
+    }
+    @Test
+    fun logClassSaveText() {
+        var log = Log()
+        log.text = "This is a test text"
+        assertEquals("This is a test text", log.text)
+    }
+    @Test
+    fun logClassSaveDate() {
+        var log = Log()
+        log.date = Date(2020,4,1)
+        assertEquals(Date(2020,4,1), log.date)
+    }
+}
