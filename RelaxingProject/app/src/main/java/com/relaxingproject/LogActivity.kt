265d60e42403d6ab9f91ce0cbffdb8b36bc0530d
DESKTOP-MN2T16N\anaki <tartarknight@gmail.com>
2020-04-26 22:04:05 -0600

    DB SQLite added

--- RelaxingProject/app/src/main/java/com/relaxingproject/LogActivity.kt
+++ RelaxingProject/app/src/main/java/com/relaxingproject/LogActivity.kt
@@ -1,12 +1,20 @@
 package com.relaxingproject
 
+import android.content.ContentValues
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.relaxingproject.classes.DatabaseHelper
 import com.relaxingproject.classes.Log
+import com.relaxingproject.classes.LogTable
 import kotlinx.android.synthetic.main.logging_screen.*
 import java.util.Date
 
 class LogActivity: AppCompatActivity() {
+    private val dbHelper = DatabaseHelper(applicationContext)
+    // Gets the data repository in write mode
+    val db = dbHelper.writableDatabase
+
+
     var logs: MutableList<Log> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +27,16 @@ class LogActivity: AppCompatActivity() {
             newLog.date =  date*/
             newLog.date = dateField.text.toString()
             logs.add(newLog)
+            // Create a new map of values, where column names are the keys
+            val values = ContentValues().apply {
+                put(LogTable.LogEntry.COLUMN_NAME_TITLE, newLog.title)
+                put(LogTable.LogEntry.COLUMN_NAME_TEXT, newLog.text)
+                put(LogTable.LogEntry.COLUMN_NAME_DATE, newLog.date)
+            }
+            // Insert the new row, returning the primary key value of the new row
+            val newRowId = db?.insert(LogTable.LogEntry.TABLE_NAME, null, values)
+
+
         }
     }
 }
