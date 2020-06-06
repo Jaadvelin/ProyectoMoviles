598e3796fdfd2b41603317187243d08dc9d364e6
DESKTOP-MN2T16N\anaki <tartarknight@gmail.com>
2020-06-06 13:59:14 -0600

    Fixes and CRUD

--- RelaxingProject/app/src/main/java/com/relaxingproject/LogActivity.kt
+++ RelaxingProject/app/src/main/java/com/relaxingproject/LogActivity.kt
@@ -1,16 +1,29 @@
 package com.relaxingproject
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.relaxingproject.classes.DatabaseHelper
 import com.relaxingproject.classes.Log
 import kotlinx.android.synthetic.main.logging_screen.*
+import java.io.ByteArrayOutputStream
 
 class LogActivity: AppCompatActivity() {
     private val dbHelper = DatabaseHelper(this)
     // Gets the data repository in write mode
 	//.
+    fun getBytes(bitmap: Bitmap):ByteArray{
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
+        return stream.toByteArray()
+    }
+
+    fun getImage(image:ByteArray):Bitmap{
+        return BitmapFactory.decodeByteArray(image, 0, image.size)
+    }
+
     var logs: MutableList<Log> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +33,10 @@ class LogActivity: AppCompatActivity() {
             newLog.text = logText.text.toString()
             newLog.title = logTitle.text.toString()
             newLog.date = dateField.text.toString()
+            newLog.rating = ratingField.text.toString()
             logs.add(newLog)
-            dbHelper.addLog(newLog)
+            val image = getBytes(pngImage)
+            dbHelper.addLog(newLog, image)
             startActivity(Intent(this,MainActivity::class.java))
         }
     }
