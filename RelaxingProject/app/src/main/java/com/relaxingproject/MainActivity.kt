8b449900fc8171013ca3ce8e98f782d60cccd3fe
Ivan <ivan>
2020-06-06 16:27:11 -0600

    Visual fixes to main menu
    Main menu fixed and date now showing

--- RelaxingProject/app/src/main/java/com/relaxingproject/MainActivity.kt
+++ RelaxingProject/app/src/main/java/com/relaxingproject/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.relaxingproject
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 
 class MainActivity : AppCompatActivity() {
 
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,5 +37,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this,MedHistoryActivity::class.java))
         }
 
+        this.currentDate()
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun currentDate() {
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
+        // val formatter = DateTimeFormatter.ofPattern("d-MM-yyyy")
+        val formatted = current.format(formatter)
+        dateLbl.text = formatted
     }
 }
