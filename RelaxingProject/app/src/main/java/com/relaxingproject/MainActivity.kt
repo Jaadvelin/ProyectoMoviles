25d22fdf2a91f041ada125263c60245fb6fa54a4
Ivan <ivan>
2020-06-06 18:40:33 -0600

    Added rating listener

--- RelaxingProject/app/src/main/java/com/relaxingproject/MainActivity.kt
+++ RelaxingProject/app/src/main/java/com/relaxingproject/MainActivity.kt
@@ -3,9 +3,12 @@ package com.relaxingproject
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.hsalf.smileyrating.SmileyRating
+import com.hsalf.smileyrating.SmileyRating.OnSmileySelectedListener
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -39,6 +42,16 @@ class MainActivity : AppCompatActivity() {
 
         this.currentDate()
 
+        smileyRating.setSmileySelectedListener(OnSmileySelectedListener { type -> // You can compare it with rating Type
+            //https://github.com/sujithkanna/SmileyRating
+            //TODO
+            // log rating to database
+
+            // You can get the user rating too
+            // rating will between 1 to 5
+            val rating = type.rating
+        })
+
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -48,4 +61,6 @@ class MainActivity : AppCompatActivity() {
         val formatted = current.format(formatter)
         dateLbl.text = formatted
     }
+
+
 }
