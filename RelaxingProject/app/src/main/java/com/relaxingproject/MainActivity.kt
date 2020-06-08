a4af68fe1853419bcda3555c572148cd0b10232d
Carlos <carlos.estradaih@gmail.com>
2020-06-08 14:12:35 -0600

    added daily notifications

--- RelaxingProject/app/src/main/java/com/relaxingproject/MainActivity.kt
+++ RelaxingProject/app/src/main/java/com/relaxingproject/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.relaxingproject
 
+import android.app.*
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.relaxingproject.util.NotificationUt
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var context: Context
+    lateinit var alarmManager: AlarmManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        alarmFun()
 
         this.meditateBtn.setOnClickListener {
             startActivity(Intent(this,MeditateActivity::class.java))
@@ -29,6 +43,22 @@ class MainActivity : AppCompatActivity() {
         this.medHistBtn.setOnClickListener {
             startActivity(Intent(this,MedHistoryActivity::class.java))
         }
+    }
 
+    fun alarmFun(){
+        context = this
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val second = 86400 * 1000;
+        val intent = Intent(context, Receiver:: class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + second, pendingIntent)
+        Log.d("MainActivity", "Create : " + Date().toString())
+    }
+
+     class Receiver : BroadcastReceiver(){
+        override fun onReceive(context: Context, intent: Intent?) {
+            Log.d("MainActivity", "Receiver : " + Date().toString())
+            NotificationUt.notifFun(context)
+        }
     }
 }
